ef762ec23f17ef15f4821107d23067315953acdb
KANIZ SHALIHA AINUN NAJMA <147938663+kanizshaliha07@users.noreply.github.com>
2023-10-16 21:58:51 +0700

    Add files via upload

---
+++ pertemuan_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "-----------------------------------------------" << endl;
+    cout << "Nama : Kaniz Shaliha Ainun Najma" << endl << endl;
+    cout << "NPM : 2310631170093" << endl << endl;
+    cout << "Kelas : C - Informatika" << endl << endl;
+    cout << "Cita - Cita : CEO muda" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Prospek Kerja Luas" << endl;
+    cout << "-----------------------------------------------" << endl;
+
+        return 0;
+}
